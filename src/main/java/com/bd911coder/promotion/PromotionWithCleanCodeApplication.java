009088b3c74bdd1362c297275783431c26964972
FC Developer <fc.developer@icloud.com>
2021-06-01 09:24:33 +0300

    Project have been created.

---
+++ src/main/java/com/bd911coder/promotion/PromotionWithCleanCodeApplication.java
@@ -0,0 +1,13 @@
+package com.bd911coder.promotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionWithCleanCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotionWithCleanCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bd911coder/promotion/PromotionWithCleanCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bd911coder.promotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionWithCleanCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
