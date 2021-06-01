1c84ff03d4d1095af71e00daf753c5b956730a71
FC Developer <fc.developer@icloud.com>
2021-06-01 10:07:01 +0300

    Projects entity layer have been writed.

---
+++ src/main/java/com/bd911coder/promotion/entities/concretes/Category.java
@@ -0,0 +1,13 @@
+package com.bd911coder.promotion.entities.concretes;
+
+import com.bd911coder.promotion.entities.abstracts.Entity;
+
+public class Category implements Entity {
+
+    private int categoryId;
+    private String categoryName;
+    private String categoryDescription;
+    
+
+
+}
---
+++ src/main/java/com/bd911coder/promotion/entities/concretes/Supplier.java
@@ -0,0 +1,20 @@
+package com.bd911coder.promotion.entities.concretes;
+
+import com.bd911coder.promotion.entities.abstracts.Entity;
+
+public class Supplier implements Entity {
+
+    private int supplierId;
+    private String companyName;
+    private String contactName;
+    private String contactTitle;
+    private String address;
+    private String city;
+    private String region;
+    private String postalCode;
+    private String country;
+    private String phone;
+    private String fax;
+    private String homepage;
+
+}
